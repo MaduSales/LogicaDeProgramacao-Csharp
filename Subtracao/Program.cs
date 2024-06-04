34299e7add03f181291c3e38d6a138a22fc10b85
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-04 17:20:28 -0300

    Add files via upload

---
+++ Subtracao/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Subtracao {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Programa de subtracao!");
+    Console.WriteLine("Digite o primeiro número");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Digite o segundo número");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    int subtracao = num1 - num2;
+    Console.WriteLine("O resultado da operação é " + subtracao);
+  }
+}
