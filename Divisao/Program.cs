511ab1b8adc52775c8fa3a4fabe2ac4632d977f7
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-04 17:21:40 -0300

    Add files via upload

---
+++ Divisao/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Divisao{
+  public static void Main(string[] args){
+    Console.WriteLine("Programa de divisão!");
+    Console.WriteLine("Digite um número:");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Digite outro número:");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    int divisao = num1 / num2;
+    Console.WriteLine("O resultado da operação é: " + divisao);
+    
+  }
+}
---
+++ Multiplicacao/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Multiplicacao {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Programa de multiplicação");
+    Console.WriteLine("Escreva um número:");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Escreva um outro número: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    int resultado = num1 * num2;
+    Console.WriteLine("O resultado da operação é: " + resultado);
+  }
+}
