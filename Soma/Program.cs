ed254acdae2f53f78c46a56625885c9fc92bb7b0
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-04 17:19:51 -0300

    Add files via upload

---
+++ Soma/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Soma{
+    public static void Main(string[] args){
+        Console.WriteLine("Programa de soma");
+        Console.Write("Digite um número");
+        int numero01 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Digite outro número");
+        int numero02 = Convert.ToInt32(Console.ReadLine());
+        int soma = numero01 + numero02;
+        Console.Write("O resultado da soma é "+ soma);
+    }
+}
