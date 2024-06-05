f716c47ed0d4f2816788bed5351d4616715081bb
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-05 17:01:08 -0300

    Add files via upload

---
+++ E04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+// 4) Escreva um programa que leia três lados de um triângulo e verifique se eles formam um triângulo válido. Para isso, a soma de dois lados deve ser sempre maior que o terceiro lado. 
+
+class Triangulo {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Escreva o valor de um lado do triângulo:");
+    int lado1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ("Escreva o valor do segundo lado do triângulo:");
+    int lado2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ("Escreva o último valor lado do triângulo:");
+    int lado3 = Convert.ToInt32(Console.ReadLine());
+
+    if (lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1){
+      Console.WriteLine("É um triângulo");
+    } else {
+      Console.WriteLine("Não é um triângulo");
+    }
+  }
+}
