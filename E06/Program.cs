a07048f05dbeb65bc69a428dbcaf9817b67fc122
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-05 17:32:11 -0300

    Add files via upload

---
+++ E06/Program.cs
@@ -0,0 +1,23 @@
+﻿// 6) Escreva um programa que leia um ano e verifique se ele é bissexto. Um ano é bissexto se for divisível por 4, mas não por 100, exceto se for divisível por 400. 
+
+using System;
+
+class AnoBissexto
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Vamos descobrir se o ano é bisexto ou não");
+        Console.WriteLine("Indique um ano: ");
+        int ano = Convert.ToInt32(Console.ReadLine());
+
+        if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0))
+        {
+            Console.WriteLine("O ano é bissexto!");
+        }
+        else
+        {
+            Console.WriteLine("O ano não é bissexto");
+        }
+
+    }
+}
