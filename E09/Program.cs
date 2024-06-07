cc4cc895f89a82711909b26b036e8717b0f105ee
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-07 17:16:28 -0300

    Add files via upload

---
+++ E09/Program.cs
@@ -0,0 +1,23 @@
+﻿// 9)Escreva um programa que leia o código de um produto e informe a sua categoria: 1 a 10: Alimento não-perecível, 11 a 20: Alimento perecível, 21 a 30: Vestuário, 31 a 40: Eletrônicos, Outros: Código inválido
+
+using System;
+
+class CodigoDoProduto{
+    public static void Main(string[] args){
+        Console.WriteLine("Informe o código de um produto para saber sua categoria: números de 1 a 40");
+        int codigo = Convert.ToInt32(Console.ReadLine());
+
+        if (codigo > 1 && codigo <= 10){
+            Console.WriteLine("Alimento não-perecível");
+        } else if (codigo >= 11 && codigo <= 20){
+            Console.WriteLine("Alimento perecível");
+        } else if (codigo >= 21 && codigo <= 30){
+            Console.WriteLine("Vestuário");
+        } else if (codigo >= 31 && codigo <= 40){
+            Console.WriteLine("Eletrônicos");
+        } else {
+            Console.WriteLine("Código Inválido");
+        }
+    }
+}
+
