a2179a0274c2104178617415aa05ff0a5b70ddb2
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-07 16:36:21 -0300

    Add files via upload

---
+++ E07/Program.cs
@@ -0,0 +1,20 @@
+﻿// 7) Escreva um programa que leia a idade de uma pessoa e informe se ela é uma criança (0-12 anos), adolescente (13-17 anos), adulto (18-59 anos) ou idoso (60 anos ou mais). 
+
+using System;
+
+class Idade{
+    public static void Main(string[] args){
+        Console.WriteLine("Informe a sua idade");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        if (idade <= 12){
+            Console.WriteLine("Você deve ser uma criança! Continue estudando hein :)");
+        } else if (idade >= 13 && idade <= 17){
+            Console.WriteLine("Você deve ser um adolescente! Não desista dos seus estudos :)");
+        } else if (idade >= 18 && idade <= 59){
+            Console.WriteLine("Você já deve ser um adulto! Ainda há tempo de estudar e aprender :)");
+        } else {
+            Console.WriteLine("Você deve ser uma pessoa idosa... aposto que vive uma vida linda, você ainda pode estudar, hein? :)");
+        }
+    }
+}
