45a218502ca367d5d000476ec150a1fd3b5fef91
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-07 17:00:03 -0300

    Add files via upload

---
+++ E08/Program.cs
@@ -0,0 +1,22 @@
+﻿// 8) Escreva um programa que leia a nota de um aluno e converta-a para conceito: A (nota >= 9), B (7 <= nota < 9), C (5 <= nota < 7), D (3 <= nota < 5) e F (nota < 3).
+
+using System;
+
+class Nota{
+    public static void Main(string[] args){
+        Console.WriteLine("Insira a nota de um aluno para determinar sua classificação");
+        int nota = Convert.ToInt32(Console.ReadLine());
+
+        if (nota >= 9){
+            Console.WriteLine("A nota é: A");
+        } else if (nota >= 7 && nota < 9){
+            Console.WriteLine("A nota é: B");
+        } else if (nota >= 5 && nota < 7){
+            Console.WriteLine("A nota é: C");
+        } else if (nota >= 3 && nota < 5){
+            Console.WriteLine("A nota é: D");
+        } else {
+            Console.WriteLine("A nota é: E");
+        }
+    }
+}
