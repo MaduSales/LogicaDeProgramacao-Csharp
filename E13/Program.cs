ec01c872a886a726ff17464ae9bf49bcd2da73b2
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-08 16:56:00 -0300

    Add files via upload

---
+++ E13/Program.cs
@@ -0,0 +1,32 @@
+﻿// 13) Escreva um programa que leia o valor de uma compra e a categoria do cliente (1 para comum, 2 para associado e 3 para VIP). Aplique os seguintes descontos:
+
+// Comum: Sem desconto
+
+// Associado: 10% de desconto
+
+// VIP: 20% de desconto
+
+// Informe o valor final da compra.
+
+using System;
+
+class CompraDoCliente{
+    public static void Main(string[] args){
+        Console.WriteLine("Insira o valor da compra: ");
+        int compra = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Agora insira a categoria do cliente, de 1 a 3:");
+        int categoria = Convert.ToInt32(Console.ReadLine());
+
+        double valorFinal = compra;
+
+        if (categoria == 1){
+            Console.WriteLine("Como o nível do cliente é baixo, o valor da compra permanece como " + valorFinal);
+        } else if (categoria == 2){
+            valorFinal = compra * 0.10;
+            Console.WriteLine("O valor final da compra é " + valorFinal);
+        } else if (categoria == 3){
+            valorFinal = compra * 0.20;
+            Console.WriteLine("O valor final da compra é " + valorFinal);
+        }
+    }
+}
